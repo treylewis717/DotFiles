5777cde6a93286c26d2d8b2f445b686a0cf0dd47
SuperMrDoggyDog <treylewis717@gmail.com>
2021-02-12 01:09:26 -0600

    Updated a couple of things

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -320,6 +320,7 @@ myStartupHook = do
         spawnOnce "volumeicon &"
         spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 0 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
         spawnOnce "kdeconnect-indicator"
+        spawnOnce "crd --start"
 
 
 
